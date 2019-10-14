36ea446519c9c255d6b25a67a3dff6416c818e5e
Andrin Gautschi <andrin.gautschi@bluewin.ch>
2019-10-14 18:16:02 +0200

    moved number into sevensegment because the test want that to be the structure

--- printout/sevensegment.cpp
+++ printout/sevensegment.cpp
@@ -1,7 +1,110 @@
 #include <ostream>
 #include <vector>
 #include <functional>
-#include "number.h"
+
+namespace RenderedDigit {
+    const int amount_of_lines = 5;
+    const int length_of_line = 3;
+
+    const std::vector<std::string> minus = {
+            "   ",
+            "   ",
+            "---",
+            "   ",
+            "   "
+    };
+    const std::vector<std::string> zero = {
+            " - ",
+            "| |",
+            "   ",
+            "| |",
+            " - "
+    };
+    const std::vector<std::string> one = {
+            "   ",
+            "  |",
+            "   ",
+            "  |",
+            "   "
+    };
+    const std::vector<std::string> two = {
+            " - ",
+            "  |",
+            " - ",
+            "|  ",
+            " - "
+    };
+    const std::vector<std::string> three = {
+            " - ",
+            "  |",
+            " - ",
+            "  |",
+            " - "
+    };
+    const std::vector<std::string> four = {
+            "   ",
+            "| |",
+            " - ",
+            "  |",
+            "   "
+    };
+    const std::vector<std::string> five = {
+            " - ",
+            "|  ",
+            " - ",
+            "  |",
+            " - "
+    };
+    const std::vector<std::string> six = {
+            " - ",
+            "|  ",
+            " - ",
+            "| |",
+            " - "
+    };
+    const std::vector<std::string> seven = {
+            " - ",
+            "  |",
+            "   ",
+            "  |",
+            "   "
+    };
+    const std::vector<std::string> eight = {
+            " - ",
+            "| |",
+            " - ",
+            "| |",
+            " - "
+    };
+    const std::vector<std::string> nine = {
+            " - ",
+            "| |",
+            " - ",
+            "  |",
+            " - "
+    };
+    const std::vector<std::string> big_e = {
+            "---",
+            "|  ",
+            "|- ",
+            "|  ",
+            "---"
+    };
+    const std::vector<std::string> small_r = {
+            "   ",
+            "   ",
+            " --",
+            "|  ",
+            "|  "
+    };
+    const std::vector<std::string> small_o = {
+            "   ",
+            "   ",
+            " - ",
+            "| |",
+            " - "
+    };
+}
 
 std::vector<std::string> resolve7SectorDigit(char digit) {
     switch (digit) {
